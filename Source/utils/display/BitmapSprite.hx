8c161a5374970482d2d75d629268e228255b5317
saumya <2saumyaray@gmail.com>
2016-06-11 00:28:54 +0530

    bitmaps are clickable now

--- Source/utils/display/BitmapSprite.hx
+++ Source/utils/display/BitmapSprite.hx
@@ -4,6 +4,12 @@ package utils.display;
 import openfl.display.Sprite;
 import openfl.display.Bitmap;
 
+import openfl.events.MouseEvent;
+
+import motion.Actuate;
+import motion.easing.Quad;
+
+
 class BitmapSprite extends Sprite {
 	var map:Bitmap;
 	var isAtCenter:Bool;
@@ -21,6 +27,17 @@ class BitmapSprite extends Sprite {
 			map.x = (-1)*(map.width/2);
 			map.y = (-1)*(map.height/2);
 		}
+		this.addEventListener(MouseEvent.CLICK,onClick);
+		//
 		this.addChild(map);
+	}
+
+	private function onClick(e:MouseEvent):Void {
+
+		var r:Float = Math.random() * 100;
+		var a:Float = r+Math.sin(Math.random()*90) + this.x;
+		var b:Float = r*Math.cos(Math.random()*90) + this.y;
+
+		Actuate.tween(this,1.0,{ x:a, y:b });
 	}
 }
